675651f4c32979c1d8250db1f3337045be05e68f
zzhaoaa <zzhaoaa@gmail.com>
2020-10-01 15:39:23 -0400

    create retinal input flow under ocular drift

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,24 +1,61 @@
 # %%
 from tensorflow import keras
-from numpy as np
-mnist = keras.datasets.mnist
+import numpy as np
+import pdb
+from matplotlib import pyplot as plt
+import progressbar
+from tqdm import tqdm
 
-(x_train0, y_train), (x_test0, y_test) = mnist.load_data()
-x_train0, x_test0 = x_train0 / 255.0, x_test0 / 255.0
-
-imgDim=28
-time_steps=20
-
-for i in range(x_train0.shape[0]):
-    img0=x_train0[i]
-    eyeX, eyeY=0,0
-    displaceX, displaceY = np.random.normal(0, 1, time_steps),  np.random.normal(0, 1, time_steps)
-    for t in range(time_steps):
-        frameT=np.zeros((imgDim,imgDim))
-        img0(max())
-        frameT
-        frameT+=np.random.normal(0, 0.5, (imgDim,imgDim))
-        eyeX+=displaceX[t]
-        eyeY+=displaceY[t]
-        
+def generate_retinal_flow(Data,time_steps,D,NoiseRate):
+    print('generating retinal flow')
+    samples=Data.shape[0]
+    imgDim=Data.shape[1];
+    retinal_flow=np.zeros((samples, time_steps, imgDim*imgDim))
+    label=np.zeros((samples, imgDim*imgDim))
+    logStep=500
     
+    #bar = progressbar.ProgressBar(maxval=samples, \
+    #widgets=[progressbar.Bar('=', '[', ']'), '\n', progressbar.Percentage()])
+    #bar.start()
+    for i in tqdm(range(samples)):
+        img0=Data[i]
+        label[i,]=img0.flatten()
+        
+        eyeX, eyeY=0.0,0.0
+        displaceX, displaceY = np.random.normal(0, D, time_steps),  np.random.normal(0, 1, time_steps)
+        for t in range(time_steps):
+            frameT=np.zeros((imgDim,imgDim))
+            imgT=img0[max(round(eyeX),0):min(round(eyeX)+imgDim,imgDim+1),
+                      max(round(eyeY),0):min(round(eyeY)+imgDim,imgDim+1)]
+            frameT[max(round(-eyeX),0):min(round(-eyeX)+imgDim,imgDim+1),
+                      max(round(-eyeY),0):min(round(-eyeY)+imgDim,imgDim+1)]=imgT
+            frameT+=np.random.normal(0, NoiseRate, (imgDim,imgDim))
+            
+            retinal_flow[i,t,]=frameT.flatten()
+            eyeX+=displaceX[t]
+            eyeY+=displaceY[t]
+    
+            #bar.update(i+1)
+            #plt.imshow(frameT)
+            #plt.show()
+            #pdb.set_trace()
+    #bar.finish()      
+            
+    return retinal_flow, label
+        
+def create_dataset():
+    mnist = keras.datasets.mnist
+    
+    (x_train0, y_train0), (x_test0, y_test0) = mnist.load_data()
+    x_train0, x_test0 = x_train0 / 255.0, x_test0 / 255.0
+    
+    time_steps=20
+    diffussion=1.5
+    NoiseRate=0.5
+    x_train,y_train=generate_retinal_flow(x_train0,time_steps,diffussion,NoiseRate)
+    x_test,y_test=generate_retinal_flow(x_test0,time_steps,diffussion,NoiseRate)
+    
+    return x_train, y_train, x_test, y_test
+
+
+ 
--- main.py
+++ main.py
@@ -5,53 +5,40 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import pdb
+import generate_dataset as gd
 
-batch_size = 100
-input_dim = 28
-
-units = 128
-output_size = 10  # labels are from 0 to 9
-
-def build_model(allow_cudnn_kernel=True):
-    # CuDNN is only available at the layer level, and not at the cell level.
-    # This means `LSTM(units)` will use the CuDNN kernel,
-    # while RNN(LSTMCell(units)) will run on non-CuDNN kernel.
-    if allow_cudnn_kernel:
-        # The LSTM layer with default options uses CuDNN.
-        lstm_layer = keras.layers.LSTM(units, input_shape=(None, input_dim))
-    else:
-        # Wrapping a LSTMCell in a RNN layer will not use CuDNN.
-        lstm_layer = keras.layers.RNN(
-            keras.layers.LSTMCell(units), input_shape=(None, input_dim)
-        )
+input_dim=28*28
+output_dim=28*28
+units=128
+def build_LSTM_model():
+     
+    lstm_layer = keras.layers.RNN(
+        keras.layers.LSTMCell(units), input_shape=(None, input_dim)
+    )
     model = keras.models.Sequential(
         [
             lstm_layer,
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(output_size),
+            keras.layers.Dense(output_dim),
         ]
     )
     return model
 
 
-mnist = keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-sample, sample_label = x_train[0], y_train[0]
 
-pdb.set_trace()
-model = build_model(allow_cudnn_kernel=False)
+model = build_LSTM_model()
 
 model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer="sgd",
-    metrics=["accuracy"],
+    optimizer='sgd',
+    loss='mse',
+    metrics=[tf.keras.metrics.MeanSquaredError()]
 )
 
-
+batch_size = 100
+x_train, y_train,x_test, y_test=gd.create_dataset()
 model.fit(
-    x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size, epochs=3
+    x_train, y_train, validation_data=(x_test, y_test),
+    batch_size=batch_size, epochs=3
 )
 
 
